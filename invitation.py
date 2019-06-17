dc68c5b197f88ec217acc20da188b8559e8c66ee
GACiobanita <ciobanita_alex@yahoo.com>
2019-06-17 12:43:12 +0100

    Getting the latitude and longitude from each line of the .txt file
    -looking into using str.translate in order to use a table of symbols/characters to remove  characters/symbols that are not necessary

--- invitation.py
+++ invitation.py
@@ -3,9 +3,18 @@ try:
     fhand = open(filepath)
 except:
     print("Default file is used instead.") #if the input file path is wrong or the input is incorrect, or simply because the user wanted this to occur
-    fhand = open("customers.txt")
+    fhand = open("customers.txt") #takes the same amount of time regardless of file size
 
-count = 0  
+count = 0
+userID = 0
+name = "Unknown"
+lat = 0
+lon = 0
 for line in fhand:
-    count+=1
-print('There were', count, 'lines.')  
+    lineContents=line.split(',')
+    
+    pos = lineContents[0].find(':')
+    lat = float(lineContents[0][pos+3:len(lineContents[0])-1])
+    
+    pos = lineContents[3].find(':')
+    lon = float(lineContents[3][pos+3:len(lineContents[3])-1])
