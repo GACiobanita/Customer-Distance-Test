db0157da3233dba0dacc34c9b2969feaf7cc71ac
GACiobanita <ciobanita_alex@yahoo.com>
2019-06-17 13:01:29 +0100

    Modified distanceCalculation.py and invitation.py
    invitation.py:
    -now we use str.translate to avoid possible confusion of random numbers in code
    -it is clearer what the code does now, a table is created, then used to remove characters/symbols allowing for the number to be used
    
    distanceCalculation.py:
    -modified the .py file to a function, in preparation for its usage in the invitation.py class
    -removed prints
    -now simply returns the value to be verified in the main class

--- distanceCalculation.py
+++ distanceCalculation.py
@@ -1,21 +1,17 @@
 from math import *
 import numpy as np
 
-print("Input latitude and longitude to find the distance from Dublin(Lat:53.339428, Long:-6.257664)")
-dlat = radians(53.339428)
-dlon = radians(-6.257664)
+def calculate_distance(iLat, iLon):
+    #Dublin's location
+    dlat = radians(53.339428)
+    dlon = radians(-6.257664)
 
-try:
-    flat =input('Enter latitude:')
-    lat = float(flat)
+    lat = iLat
     lat = radians(lat)
-    flong = input('Enter longitude:')
-    lon = float(flong)
+    lon = iLon
     lon = radians(lon)
-except:
-    print('We need numbers for the formula to work.')
 
-x = sin(lat) * sin(dlat) + cos(lat) * cos(dlat) * cos(abs(dlon - lon))
-dist = acos(x)
-dist = 6371 * dist #convert to km
-print("The distance between Dublin and the input latitude and longitude is:", dist)
+    x = sin(lat) * sin(dlat) + cos(lat) * cos(dlat) * cos(abs(dlon - lon))
+    dist = acos(x)
+    dist = 6371 * dist #convert to km
+    return dist
--- invitation.py
+++ invitation.py
@@ -5,6 +5,8 @@ except:
     print("Default file is used instead.") #if the input file path is wrong or the input is incorrect, or simply because the user wanted this to occur
     fhand = open("customers.txt") #takes the same amount of time regardless of file size
 
+remove = str.maketrans(dict.fromkeys(' :"'))#create a dictionary from keys, then create a table "remove"
+
 count = 0
 userID = 0
 name = "Unknown"
@@ -14,7 +16,12 @@ for line in fhand:
     lineContents=line.split(',')
     
     pos = lineContents[0].find(':')
-    lat = float(lineContents[0][pos+3:len(lineContents[0])-1])
+    tempVal = lineContents[0][pos:len(lineContents[0])-1]
+    tempVal = tempVal.translate(remove) #use the table to create a new string, we still use tempVal in this case, removing the characters in the table from the string
+    lat = float(tempVal)                #this could be futher extended to also remove all the letters from the start, leaving us only the numbers we need in the sections of the list we are interested
     
     pos = lineContents[3].find(':')
-    lon = float(lineContents[3][pos+3:len(lineContents[3])-1])
+    tempVal = lineContents[3][pos:len(lineContents[3])-1]
+    tempVal = tempVal.translate(remove)
+    lon = float(tempVal)
+
