bdd5ded0349e7e1096e2effd5b304278c30f2346
GACiobanita <ciobanita_alex@yahoo.com>
2019-07-05 13:18:07 +0300

    Created TestBST

---
+++ TestBST.py
@@ -0,0 +1,21 @@
+import unittest
+import random
+from BST import BST
+from Node import Node
+from Customer import Customer
+
+class TestBST(unittest.TestCase):
+    def setUp(self):
+        self.tree=BST()
+        for i in range(5,10):
+            testCustomer=Customer(random.randint(1, 101), "Customer"+i, lat=i, lon=i+1)
+            self.tree.insert(testCustomer)
+        for i in range(0,5):
+            testCustomer=Customer(random.randint(1, 101), "Customer"+i, lat=i, lon=i+1)
+            self.tree.insert(testCustomer)
+
+    def test_tree(self):
+        treeContents=[]
+        treeContents=self.tree.inorder_traversal(self.tree.root)
+        self.assertEqual(10, len(treeContents))
+        self.assertIsNot(0 , len(treeContents))
