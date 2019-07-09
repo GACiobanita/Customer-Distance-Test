accda0acb983c3ffea15f01b9b9d9a53383e459b
GACiobanita <ciobanita_alex@yahoo.com>
2019-07-09 17:42:17 +0300

    Spreading the responsabilities of user_inviter to an organizer class for final stages of the project

---
+++ implementation/organizer.py
@@ -0,0 +1,25 @@
+import json
+import os
+
+class Organizer(object):
+
+    def __init__(self):
+        self.ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+        self.CUSTOMER_FILE = self.ROOT_DIR + '\\' + "customers.txt"
+        self.input_file_path = self.CUSTOMER_FILE
+
+    def acquire_filepath(self):
+        input_path = input("Enter the path leading towards the desired file: ")
+        if os.path.exists(input_path) is True:
+            self.input_file_path = input_path
+        else:
+            print("File path is incorrect, default file will be used instead.")
+
+    def read_from_filepath(self):
+        with open(self.input_file_path) as jsonFile:
+            for line in jsonFile:
+                try:
+                    self.fileData.append(json.loads(line))
+                except ValueError:
+                    print("Encountered error for line: " + line)
+                    continue
